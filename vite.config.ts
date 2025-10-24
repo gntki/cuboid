7156eb756fdcf32b8a394f16d95b4f0ef5c1086b
a.developer <developer@developer.ru>
2025-10-24 19:09:12 +0300

    chore(vite.config): настройка деплоя

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    base: process.env.NODE_ENV === 'production' ? '/cuboid/' : './',
-    build: {
-        outDir: 'dist',
-        assetsDir: 'assets'
-    },
+    base: '/cuboid/',
     plugins: [
         react({
             babel: {
